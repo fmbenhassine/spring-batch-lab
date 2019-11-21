bdd748fa64cf1721ffdc251c3cbf97189cb78652
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2019-11-21 12:02:35 +0100

    Add sample app for https://jira.spring.io/browse/BATCH-2351

--- batch2351/src/main/java/com/example/demo/DemoApplication.java
+++ batch2351/src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,56 @@
 package com.example.demo;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
-@SpringBootApplication
-@EnableBatchProcessing
-@ImportResource(value={"classpath:job-context.xml"})
+// BATCH-2351: When using java config and importing XML, step/job scoped beans don't get proxied:
+
+// the following example uses Java config and imports XML, and yet step/job scoped beans get proxied as expected
+
+@Configuration
+@ImportResource(value={"classpath:job-context.xml"}) // the job is configured in XML
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	// infrastructure beans are configured in Java
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new ResourcelessTransactionManager();
+	}
+
+	@Bean
+	public JobRepository jobRepository() throws Exception {
+		MapJobRepositoryFactoryBean mapJobRepositoryFactoryBean = new MapJobRepositoryFactoryBean();
+		mapJobRepositoryFactoryBean.setTransactionManager(transactionManager());
+		return mapJobRepositoryFactoryBean.getObject();
+	}
+
+	@Bean
+	public JobLauncher jobLauncher() throws Exception {
+		SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+		jobLauncher.setJobRepository(jobRepository());
+		return jobLauncher;
+	}
+
+	public static void main(String[] args) throws Exception {
+		ApplicationContext context = new AnnotationConfigApplicationContext(DemoApplication.class);
+		JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+		Job job = context.getBean(Job.class);
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addString("param", "foo")
+				.toJobParameters();
+		jobLauncher.run(job, jobParameters);
 	}
 }
