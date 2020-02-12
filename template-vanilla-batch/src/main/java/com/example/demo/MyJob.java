3881ca93930955c3df3c9a45fe197fade0d9e86f
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2020-02-12 18:20:14 +0100

    Add template with vanilla batch

---
+++ template-vanilla-batch/src/main/java/com/example/demo/MyJob.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class MyJob {
+
+	@Bean
+	public Job job(JobBuilderFactory jobs, StepBuilderFactory steps) {
+		return jobs.get("job")
+				.start(steps.get("step")
+						.tasklet((contribution, chunkContext) -> {
+							System.out.println("hello world");
+							return RepeatStatus.FINISHED;
+						})
+						.build())
+				.build();
+	}
+
+	public static void main(String[] args) throws Exception {
+		ApplicationContext context = new AnnotationConfigApplicationContext(MyJob.class);
+		JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+		Job job = context.getBean(Job.class);
+		jobLauncher.run(job, new JobParameters());
+	}
+
+}
